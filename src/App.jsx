a197ce65dc7efb3f45503cbbe0d028a3767a47e6
Miwa G <miwa.guhres@hotmail.com>
2024-09-10 15:39:26 +0200

    First test link from component Repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import {Login} from "fwk-22-a-components";
+import {Login, NavBar} from "fwk-22-a-components";
 
 function App() {
 
 
   return (
     <>
+    <NavBar/>
     <Login/>
         
     </>
